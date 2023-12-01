ffff47b29ec904a7b385ffd0573cba438bcf87ab
Betty Sosterics <betty.sosterics@gmail.com>
2023-12-01 13:00:28 +0100

    Remove mapping of projects

--- app/components/ProjectsSection.tsx
+++ app/components/ProjectsSection.tsx
@@ -4,32 +4,6 @@ import React from 'react';
 import { BsArrowUpRightSquare, BsGithub } from 'react-icons/bs';
 import SlideUp from './SlideUp';
 
-const projects = [
-  {
-    name: 'Bandify',
-    description:
-      'Bandify is an all-in-one music collaboration app for musicians.',
-    image: '/bandify.png',
-    github: 'https://github.com/bettysosterics/upleveled-final-project',
-    link: 'https://bandify-project.fly.dev/',
-  },
-  // {
-  //   name: 'PlatoIO',
-  //   description: 'PlatoIO is a to do list app that built using the PERN stack.',
-  //   image: '/platoio.png',
-  //   github: 'https://github.com/hqasmei/platoio',
-  //   link: 'https://platoio.com/register',
-  // },
-  // {
-  //   name: 'Kator Family Photos',
-  //   description:
-  //     'Kator Family Photos is a photos and video digitization service in the LA area.',
-  //   image: '/familyphotos.png',
-  //   github: 'https://github.com/hqasmei/katorfamilyphotos',
-  //   link: 'https://katorfamilyphotos.com/',
-  // },
-];
-
 export default function ProjectsSection() {
   return (
     <section id="projects">
@@ -39,56 +13,47 @@ export default function ProjectsSection() {
       </h1>
 
       <div className="flex flex-col space-y-28">
-        {projects.map((project, idx) => {
-          return (
-            <div key={idx}>
-              <SlideUp offset="-300px 0px -300px 0px">
-                <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
-                  <div className=" md:w-1/2">
-                    <Link
-                      href="https://bandify-project.fly.dev/"
-                      target="_blank"
-                    >
-                      <Image
-                        src={project.image}
-                        alt=""
-                        width={1000}
-                        height={1000}
-                        className="rounded-xl shadow-xl hover:opacity-70"
-                      />
-                    </Link>
-                  </div>
-                  <div className="mt-8 md:w-1/2">
-                    <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
-                    <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
-                      {project.description}
-                    </p>
-                    <div className="flex flex-row align-bottom space-x-4">
-                      <Link
-                        href="https://github.com/bettysosterics/upleveled-final-project"
-                        target="_blank"
-                      >
-                        <BsGithub
-                          size={30}
-                          className="hover:-translate-y-1 transition-transform cursor-pointer"
-                        />
-                      </Link>
-                      <Link
-                        href="https://bandify-project.fly.dev/"
-                        target="_blank"
-                      >
-                        <BsArrowUpRightSquare
-                          size={30}
-                          className="hover:-translate-y-1 transition-transform cursor-pointer"
-                        />
-                      </Link>
-                    </div>
-                  </div>
+        <div>
+          <SlideUp offset="-300px 0px -300px 0px">
+            <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+              <div className=" md:w-1/2">
+                <Link href="https://bandify-project.fly.dev/" target="_blank">
+                  <Image
+                    src="/bandify.PNG"
+                    alt=""
+                    width={1000}
+                    height={1000}
+                    className="rounded-xl shadow-xl hover:opacity-70"
+                  />
+                </Link>
+              </div>
+              <div className="mt-8 md:w-1/2">
+                <h1 className="text-4xl font-bold mb-6">Bandify</h1>
+                <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                  Bandify is an all-in-one music collaboration app for
+                  musicians.
+                </p>
+                <div className="flex flex-row align-bottom space-x-4">
+                  <Link
+                    href="https://github.com/bettysosterics/upleveled-final-project"
+                    target="_blank"
+                  >
+                    <BsGithub
+                      size={30}
+                      className="hover:-translate-y-1 transition-transform cursor-pointer"
+                    />
+                  </Link>
+                  <Link href="https://bandify-project.fly.dev/" target="_blank">
+                    <BsArrowUpRightSquare
+                      size={30}
+                      className="hover:-translate-y-1 transition-transform cursor-pointer"
+                    />
+                  </Link>
                 </div>
-              </SlideUp>
+              </div>
             </div>
-          );
-        })}
+          </SlideUp>
+        </div>
       </div>
     </section>
   );
